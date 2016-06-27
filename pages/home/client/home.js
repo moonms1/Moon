c5b09ab1fd0b0bc8a433186d0b13a6ae3ae599da
Sung Jun Moon <moonms1@turing.cs-i.brandeis.edu>
2016-06-27 19:16:42 -0400

    check errors

---
+++ pages/home/client/home.js
@@ -0,0 +1,40 @@
+Template.home.onCreated(function() {
+  this.state = new ReactiveDict();
+  this.state.setDefault({
+    color: "bg-info",
+    counter: 0,
+
+  });
+  console.log("creating the template");
+  console.dir(this.state);
+});
+
+
+Template.home.helpers({
+theColor: function(){
+const instance = Template.instance();
+return instance.state.get("color");
+},
+theCounter: function(){
+const instance = Template.instance();
+return instance.state.get("counter");
+}
+
+
+})
+
+
+Template.home.events({
+
+"change .js-color": function(event, instance){
+const c = instance.$(".js-color").val();
+instance.state.set("color",c);
+},
+
+"click .js-pusher": function(event, instance){ 
+console.log("Clicked");
+const p = instance.state.get("counter"); 
+instance.state.set("counter", 1+p); 
+},
+
+});
--- server/startup.js
+++ server/startup.js
@@ -1,8 +1,8 @@
 Meteor.startup(function(){
-	if(Quizzes.find().count()==0){
-		//create these fixtures
-		Quizzes.insert({writtenBy:"Moon",genre:"nonsense",difficulty:[3]});
-		Quizzes.insert({writtenBy:"Moon1",genre:"math",difficulty:[1]});
-		Quizzes.insert({writtenBy:"Moon2",genre:"computerscience",difficulty:[2]});
-	}
+ Quizzes.remove({});
+ Quizzes.insert({question:"Who was the legendary Benedictine monk who invented champagne?",answer:"DomPerignon"});
+ Quizzes.insert({question:"Name the largest freshwater lake in the world?",answer:"Lake Superior"});
+ Quizzes.insert({question:"Where would you find the sea of Tranquility?",answer:"The Moon"});
+ Quizzes.insert({question:"This five letter word gets shorter when you add two letters to it.",answer:"short"});
+ Quizzes.insert({question:"I have seas without water. I have forests without wood. I have deserts without sand. I have houses with no brick. What am I?",answer:"map"});
 })
